91f00cb8515130b86dd8c18b2e07845b105a9cf1
DiyorbekTursunov <diyorbektursunov570@gmail.com>
2025-05-12 12:55:06 +0500

    fixed shit

--- src/controller/employee/employee.create.controller.ts
+++ src/controller/employee/employee.create.controller.ts
@@ -8,7 +8,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "default_secret";
 
 // Create user and employee
 export const createEmployee = async (req: Request, res: Response) => {
-  const { login, password, role, departmentId } = req.body;
+  const { login, password, name, role, departmentId } = req.body;
 
   // Validate required fields
   if (!login) return res.status(400).json({ error: "Login is required" });
@@ -16,6 +16,7 @@ export const createEmployee = async (req: Request, res: Response) => {
   if (!role) return res.status(400).json({ error: "Role is required" });
   if (!departmentId)
     return res.status(400).json({ error: "Department ID is required" });
+  if (!name) return res.status(400).json({ error: "Name is required" });
 
   try {
     // Check if user with the same login already exists
@@ -45,6 +46,12 @@ export const createEmployee = async (req: Request, res: Response) => {
         login,
         password: hashedPassword,
         role,
+        Employee: {
+          create: {
+            name: department.name,
+            departmentId,
+          },
+        },
       },
       include: {
         Employee: {
